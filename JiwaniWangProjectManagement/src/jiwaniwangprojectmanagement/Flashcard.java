502f69042eb3cc6b38ae133047a3da4e036623ee
edaword <53409587+edaword@users.noreply.github.com>
2021-03-23 09:32:59 -0400

    Add flashcard class

---
+++ JiwaniWangProjectManagement/src/jiwaniwangprojectmanagement/Flashcard.java
@@ -0,0 +1,62 @@
+/*
+    Class that represents a flash card
+ */
+package jiwaniwangprojectmanagement;
+
+/**
+ *
+ * @author edwan2505
+ */
+public class Flashcard {
+    private String question, answer;
+    private boolean reverse;
+    
+    /**
+     * instantiate flashcard without reverse
+     * @param question - the question to ask
+     * @param answer - the answer to the question
+     */
+    public Flashcard (String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+        reverse = false;
+    }
+    
+    /**
+     * instantiate flashcard with explicit reverse or not
+     * @param question - the question to ask
+     * @param answer - the answer to the question
+     * @param reverse - whether they will be displayed in reverse or not
+     */
+    public Flashcard (String question, String answer, boolean reverse) {
+        this(question, answer);
+        this.reverse = reverse;
+    }
+    
+    public String getQuestion() {
+        return question;
+    }
+    public String getAnswer() {
+        return answer;
+    }
+    public boolean getReverse() {
+        return reverse;
+    }
+    
+    public void setReverse(boolean newReverse) {
+        reverse = newReverse;
+    }
+    
+    public Flashcard clone () {
+        Flashcard f2 = new Flashcard(question, answer, reverse);
+        return f2;
+    }
+    public boolean equals(Flashcard f) {
+        return this.question==f.question && this.answer==f.answer && this.reverse==f.reverse;
+    }
+    public String toString() {
+        return "Question: " + question + 
+                "\nAnswer: " + answer +
+                "\nReverse: " + reverse;
+    }
+}
